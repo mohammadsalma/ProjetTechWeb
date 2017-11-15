5d140ea483ca43c44f9900f91d2d72082e3e1277
mohammadsalma <msalma@si5.fdsetu.infra.umontpellier.fr>
2017-11-15 15:26:23 +0100

    ajouter des commentaires

--- initial_bddBotanique.php
+++ initial_bddBotanique.php
@@ -1,7 +1,14 @@
 <?php
+// Pour inclure le fichier bdd.php qui contient les fonctions nécessaires
+// ainsi les données de login
 include "bdd.php";
+// Pour se connecter à la base de données
 connexionbd();
+
+// Pour la creation de table
 creation_table();
+
+//Pour l'insertion des données d'exemples
 insertion_exemples();
 
 ?>
