eeb7d674be42ac062bc88d627aefb54c9e9047ec
5dith <33690294+5dith@users.noreply.github.com>
2017-11-15 14:22:59 +0100

    Update and rename Main.html to main.php

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+include"entete.html"
+	<body>
+		<h1>Gâteau au chocolat</h1>
+
+		<h2>Ingrédients pour 8</h2>
+
+		<h2>Préparation</h2>
+		<p>
+
+		</p>
+
+		<h2>Images</h2>
+		<img src="gateau chocolat.jpg" alt="Image à la noix"/>
+
+
+		<h2>Autres recettes</h2>
+		
+	</body>
+include "pied.html"
