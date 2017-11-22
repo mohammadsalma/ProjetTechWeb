c6c3ca60911be6f61489e15a8d24e9fa9b398cc3
5dith <33690294+5dith@users.noreply.github.com>
2017-11-22 16:42:59 +0100

    Update and rename listedynamique.js to listedynamique.php

---
+++ listedynamique.php
@@ -0,0 +1,15 @@
+$.ajax({
+
+   /**
+
+    * Nous réutilisons $.ajax() pour illustrer l'exemple
+
+    */
+
+
+    // Concentrons nous sur le paramètres data
+
+    data : 'valeur1=' + valeur1 + '&valeur2=' + valeur2 + '&valeur3=' + valeur3,
+
+
+});
