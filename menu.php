5e5008e8c1b2ce0779b50387b34d94e9af62870a
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-11-22 16:11:50 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,5 @@
 <nav id="menu">
 	<div class="element_menu">
-		<h3>Navigation</h3>
 		<ul>
 			<li><a href="listedonnees.php">Consulter la base de données</a></li>
 			<li><a href="#">Lien 2</a></li>
