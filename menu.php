fd52f59a4630e5d18c6554ad4be30d81f0af7845
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-11-22 13:55:08 +0100

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,13 @@
+<nav id="menu">
+	<div class="menu">
+		<h3>Navigation</h3>
+		<ul>
+			<li><a href="page1.html">Lien</a></li>
+			
+			<li><a href="page2.html">Lien2</a></li>
+			
+			<li><a href="page3.html">Lien3</a></li>
+			
+			</ul>
+	</div>
+</nav>
