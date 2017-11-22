f0cd7f3a9bcb3e9b33ad787aef5ddbb453e6b821
5dith <33690294+5dith@users.noreply.github.com>
2017-11-22 15:05:20 +0100

    Update listedonnees.php

--- listedonnees.php
+++ listedonnees.php
@@ -1,15 +1,21 @@
 <?php   
 
 include "bdd.php";// inclusion du fichier a lire
-//connexion
 $base= connexionbd();// me permet de me connecter a la base de donnée car ele contient toutes les informations
 $req= 'Select * from releves';
 $requete=requete($base,$req);
-$ligne=mysqli_fetch_row($requete);
-echo "$requete";
+//echo "$requete";
+//print_r($requete);
+// lecture du resultat
+//acceder a des colonnes ligne/ligne(le echo dans la boucle)
 
+// extraire 
+foreach ($requete as $ligne) {
+//echo $ligne['id'],$ligne['nom_plante'],$ligne['lieu'],$ligne['latitude'],$ligne['longitude'],$ligne['date_releve'],$ligne['photo'],$ligne['nom_collecteur'],$ligne['prenom_collecteur'],$ligne['commentaire'];
+	echo "<label>Nom: </label>";
+	echo $ligne['nom_plante'];
+	echo "<br>";
+}
 
 
 ?>
-
-
