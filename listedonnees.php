3501f01edba417ed510ec30190ae0796e42ea6d2
5dith <33690294+5dith@users.noreply.github.com>
2017-11-22 14:18:02 +0100

    Update listedonnees.php

--- listedonnees.php
+++ listedonnees.php
@@ -1,47 +1,11 @@
 <?php   
+
 include "bdd.php";// inclusion du fichier a lire
-requete($bd,$req);    
-?>
-<!--  creation d'un tableu vide-->
-<table class="">
-<caption>donnees de relevés botaniques</caption>
-<tr>
-    <th><p class="text-error"></p></li>
-    <th><p class="text-error"></p></th>
-    <th><p class="text-error"></p></th>
-    <th><p class="text-error"></p></th>
-	  <th><p class="text-error"></p></th>
-  	<th><p class="text-error"></p></th>
-   
-</tr>
-<tr>
-<!--  affectation des valeurs contenu dans la BDD-->
-<? while($row = $req->fetch()) { ?>
-        <td><? echo $row['']; ?></td>
-        <td><? echo $row['']; ?></td>
-        <td><? echo $row['']; ?></td>
-        <td><? echo $row['']; ?></td>
-    
-				<td><? if ($row[''] == "0") { 
-					echo ''; 
-					} 
-				if ($row[''] == "1") { 
-					echo '<p class="text-warning"><strong></strong></p>'; 
-					} 
-			
-				if ($row[''] == "2") { 
-					echo '<p class="text-error"><strong></strong></p>'; 
-				}?></td>
-    
-				<td>
-				<? echo '<a class=""><a>
-				<br/>
-				'; ?>
-				</td> 
-</tr>
-<!--  commandes a verifier-->
-<? }   
-$req->closeCursor();   
+//connexion
+$base= connexionbd();// me permet de me connecter a la base de donnée car ele contient toutes les informations
+$req= 'Select * from releves';
+$requete=requete($base,$req);
+$ligne=mysqli_fetch_row($requete);
 ?>
 
-</table>
+
