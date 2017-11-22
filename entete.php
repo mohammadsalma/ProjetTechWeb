cdb0018d50e6c9598bf50fac75076236754f12b9
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-11-22 14:14:08 +0100

    Update entete.php

--- entete.php
+++ entete.php
@@ -1,5 +1,14 @@
-<header id="entete">
-	<center>
-		<img src="https://image.freepik.com/free-icon/plant-and-root_318-41579.jpg" alt="logo">
-	</center>
-</header>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Projet 2017 - Techno du Web</title>
+	</head>
+
+	<body>
+
+		<header id="entete">
+			<center>
+				<img src="https://image.freepik.com/free-icon/plant-and-root_318-41579.jpg" alt="logo">
+			</center>
+		</header>
