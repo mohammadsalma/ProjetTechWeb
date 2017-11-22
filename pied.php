f952afad4af7bf9c1bcfcd6ecd5b53b1a02d9fc0
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-11-22 14:14:21 +0100

    Update pied.php

--- pied.php
+++ pied.php
@@ -1,7 +1,11 @@
-<footer id="pied-de-page">
-	<p>Copyright: <b>Cremaschi Julie | Endiesseu Edith | Salma Mohammad</b>
-	Etudiants du Master BCD 2e année - Bioinformatique, Connaissances, Données<br>
+	<footer id="pied-de-page">
+		<p>Copyright: <b>Cremaschi Julie | Endiesseu Edith | Salma Mohammad</b>
+		Etudiants du Master BCD 2e année - Bioinformatique, Connaissances, Données<br>
 
-	<u>Dernière mise à jour du site</u>: 22/11/2017</br>
-	</p>
-</footer>
+		<u>Dernière mise à jour du site</u>: 22/11/2017</br>
+		</p>
+	</footer>
+
+	</body>
+
+</html>
