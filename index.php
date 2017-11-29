6184b9ddb6490dee5aa7da4caf463bd2a7e3a2bd
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-11-29 16:15:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,27 @@
-<!-- entete -->	
-<?php include("entete.php"); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Projet 2017 - Techno du Web</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="text/javascript" src="effet.js">
+	</head>
 
-<hr>
+	<body>
 
-<!-- corps -->
-	<br>
-	<div id="bienvenue">
-		<h1>Bienvenue sur notre application</h1>
+		<header id="entete">
+			<center>
+				<img src="https://image.freepik.com/free-icon/plant-with-leaves_318-56717.jpg" width="10%" alt="logo" id="ancre">
+			</center>
+		</header>
 
-		<p>Cette application web va vous permettre de consulter gratuitement et rapidement des relevés botaniques. 
-			Connectez-vous pour ajouter ou supprimer des relevés.</p>
-	</div>
-	<br>
-	<div id="login">
-		<p>Pensez à vous identifier :</p>
-
-		<form action="main.html" method="post">
-
-		<p>
-			<label>Username</label>
-			<input type="text" placeholder="Identifiant" name="username" required>
-			<label>password :</label>
-			<input type="password" name="password" />
-			<input type="submit" value="Valider" />
-		</p>
-		</form>
-	</div>
-	<br>
-	<div id="corps">
-		<center>
-			[corps de la page]<br>
-		</center>
-	</div>
-	<br>
-	
-<!-- pied de page -->
-<?php include("pied.php"); ?>
+		<nav id="menu">
+			<div class="element_menu">
+			<ul>
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="listedonnees.php">Consulter la base de données</a></li>
+				<li><a href="listedynamique.html">Liste dynamique</a></li>
+				<li><a href="#">Lien 3</a></li>	
+			</ul>
+			</div>
+		</nav>
