034bfe08e3ff4f62e335ec69fbe57d57c7219642
mohammadsalma <mohammad.salma@hotmail.fr>
2017-11-29 14:15:25 +0100

    premier commit by komodo

--- listedynamique.php
+++ listedynamique.php
@@ -8,43 +8,43 @@ function getList(){
 $req= 'Select * from releves';
 $requete=requete($base,$req);
 
-// lecture du resultat
-echo"<table>";
-echo"<tr><th> Nom</th><th> Lieu</th>
-		<th> Latitude</th>
-		<th> Date du releve</th>
-		<th> Photo</th>
-		<th> Nom du collecteur</th>
-		<th> Prenom du collecteur</th>
-		<th> Commentaire</th>
-		</tr>";
+//// lecture du resultat
+//echo"<table>";
+//echo"<tr><th> Nom</th><th> Lieu</th>
+//		<th> Latitude</th>
+//		<th> Date du releve</th>
+//		<th> Photo</th>
+//		<th> Nom du collecteur</th>
+//		<th> Prenom du collecteur</th>
+//		<th> Commentaire</th>
+//		</tr>";
 // Pour acceder a des colonnes ligne/ligne(le echo dans la boucle)
-foreach ($requete as $ligne) {
-	echo "<tr><td>",$ligne['nom_plante']," </td><td> ",
-	$ligne['lieu']," </td><td> ",
-	$ligne['latitude'], " </td><td> ",
-	$ligne['date_releve'], " </td><td> ",
-	"<img src=",$ligne['photo'],">"," </td><td> ",
-	$ligne['nom_collecteur'], " </td><td> ",
-	$ligne['prenom_collecteur'], " </td><td> ",
-	$ligne['commentaire'], " </td>",
-	"</tr>";
-	
-	
-	
-	/*echo "<label>lieu: </label>";
-	echo $ligne['lieu'];
-	echo "<label>latitude: </label>";
-	echo $ligne['latitude'];
-	echo "<label>date_releve: </label>";
-	echo $ligne['date_releve'];
-	echo "<label>photo: </label>";
-	echo "<img src=",$ligne['photo'],">";*/
-	
-	//echo "<br>";
-}
+//foreach ($requete as $ligne) {
+//	echo "<tr><td>",$ligne['nom_plante']," </td><td> ",
+//	$ligne['lieu']," </td><td> ",
+//	$ligne['latitude'], " </td><td> ",
+//	$ligne['date_releve'], " </td><td> ",
+//	"<img src=",$ligne['photo'],">"," </td><td> ",
+//	$ligne['nom_collecteur'], " </td><td> ",
+//	$ligne['prenom_collecteur'], " </td><td> ",
+//	$ligne['commentaire'], " </td>",
+//	"</tr>";
+//	
+//	
+//	
+//	/*echo "<label>lieu: </label>";
+//	echo $ligne['lieu'];
+//	echo "<label>latitude: </label>";
+//	echo $ligne['latitude'];
+//	echo "<label>date_releve: </label>";
+//	echo $ligne['date_releve'];
+//	echo "<label>photo: </label>";
+//	echo "<img src=",$ligne['photo'],">";*/
+//	
+//	//echo "<br>";
+//}
 
-echo "</table>"; 
+//echo "</table>"; 
    
 }
 
