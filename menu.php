249dd357cfa69f966e40e2ea3dafbfeba7467877
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-11-29 16:16:41 +0100

    Delete menu.php

--- menu.php
+++
@@ -1,9 +0,0 @@
-<nav id="menu">
-	<div class="element_menu">
-		<ul>
-			<li><a href="listedonnees.php">Consulter la base de données</a></li>
-			<li><a href="listedynamique.html">Lien 2</a></li>
-			<li><a href="#">Lien 3</a></li>	
-		</ul>
-	</div>
-</nav>
