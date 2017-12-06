2bd436f2c7af68bb423ec1a8dc135cd890a379a8
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-12-06 15:10:23 +0100

    Update entete.php

--- entete.php
+++ entete.php
@@ -11,7 +11,7 @@
 
 		<header id="entete">
 			<center>
-				<img src="https://image.freepik.com/free-icon/plant-with-leaves_318-56717.jpg" width="10%" alt="logo" id="ancre">
+				<img src="https://image.freepik.com/free-icon/plant-with-leaves_318-56717.jpg" width="15%" alt="logo" id="ancre">
 			</center>
 		</header>
 
@@ -21,7 +21,7 @@
 				<li><a href="index.php">Accueil</a></li>
 				<li><a href="listedonnees.php">Consulter la base de données</a></li>
 				<li><a href="listedynamique.html">Liste dynamique</a></li>
-				<li><a href="#">Lien 3</a></li>	
+				<li><a href="ajoutsuppr.php">Ajouter/Supprimer des données</a></li>	
 			</ul>
 			</div>
 		</nav>
