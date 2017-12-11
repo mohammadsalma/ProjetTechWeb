09fc858a8aaab80b00ee34af227d741bcb8e846f
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-12-11 16:04:45 +0100

    Update entete.php

--- entete.php
+++ entete.php
@@ -4,14 +4,13 @@
 		<meta charset="utf-8"/>
 		<title>Projet 2017 - Techno du Web</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<link rel="text/javascript" src="effet.js">
 	</head>
 
 	<body>
 
 		<header id="entete">
 			<center>
-				<img src="https://image.freepik.com/free-icon/plant-with-leaves_318-56717.jpg" width="15%" alt="logo" id="ancre">
+				<a href="index.php"><img src="https://image.freepik.com/free-icon/plant-with-leaves_318-56717.jpg" width="15%" alt="logo" id="ancre"></a>
 			</center>
 		</header>
 
