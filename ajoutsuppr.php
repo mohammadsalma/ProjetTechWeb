962ae557dc88a187d9b37553b1751ad1e35fcfc9
mohammadsalma <mohammad.salma@hotmail.fr>
2017-12-11 18:00:28 +0100

    modifier la requet

--- ajoutsuppr.php
+++ ajoutsuppr.php
@@ -20,7 +20,7 @@
         <br>
 
         <label for="date_relevé">Date_relevé :</label>
-        <input type="text" id="date_relevé" />
+        <input type="text" id="date_releve" />
         <br>
 
         <label for="Photo">Photo :</label>
@@ -32,7 +32,7 @@
         <br>
 
         <label for="Prénom_collecteur">Prénom_collecteur:</label>
-        <input type="text" id="Prénom_collecteur" />
+        <input type="text" id="Prenom_collecteur" />
         <br>
 
         <label for="Commentaire">Commentaire :</label>
@@ -44,8 +44,35 @@
 </form>
 
 <!-- emplacement liste donnees statiquee-->
+<?php
+function addList(){
+   $motnom = $_REQUEST['Nom_plante'];
+   $motlieu = $_REQUEST['Lieu'];
+   $motalti = $_REQUEST['Latitude'];
+   $motlong = $_REQUEST['Longitude'];
+   $motdat = $_REQUEST['date_releve'];
+   $motphoto = $_REQUEST['Photo'];
+   $motn_col = $_REQUEST['Nom_collecteur'];
+   $motpre_col = $_REQUEST['Prenom_collecteur'];
+   $motComm = $_REQUEST['Commentaire'];
+   $base= connexionbd();// me permet de me connecter a la base de donnée car ele contient toutes les informations
+   $req= "INSERT INTO releves VALUES (DEFAULT, '".$motnom."', 
+                                      '".$motlieu."',
+                                        '".$motalti."',
+                                        '".$motlong."',
+                                        '".$motdat."',
+                                        '".$motphoto."',
+                                        '".$motn_col."',
+                                        '".$motpre_col."',
+                                        '".$motComm."')"
+                                       ;
+   //echo $req;                      
+   $requete=requete($base,$req);
+   return $requete;
+   
+}
 
-
-    
+$list_data = addList();
+?>    
 <!-- pied de page -->
 <?php include("pied.php"); ?>
