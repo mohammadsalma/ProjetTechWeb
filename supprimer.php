cb2bcb68b86625122e1f80eb2396ea50fa649ccd
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-12-13 12:16:17 +0100

    Update supprimer.php

--- supprimer.php
+++ supprimer.php
@@ -21,5 +21,7 @@ $req= "DELETE FROM releves WHERE id ='".$id."'";
 //echo $req;
 $requete=requete($base,$req);
 
-header('Location: listedynamique.html');
+echo 'Données supprimées avec succès !';
+
+header('Refresh: 2; url=listedynamique.html');
 ?>
