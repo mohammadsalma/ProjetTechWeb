0f03a790c580a738cb992fe2d3feedbf6ab77a2f
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-12-13 13:43:06 +0100

    Update ajout.php

--- ajout.php
+++ ajout.php
@@ -9,15 +9,9 @@ echo '
             <legend>Identifiez-vous :</legend>
               <p>
                  <label for="login">Login : </label> 
-<<<<<<< HEAD
                  <input type="text" name="login" id="login" value="test" />
                  <label for="password">Password : </label> 
                  <input type="password" name="password" id="password" value="123" />
-=======
-                 <input type="text" name="login" id="login" value="" />
-                 <label for="password">Password : </label> 
-                 <input type="password" name="password" id="password" value="" />
->>>>>>> 3c4555815039a49af935fd054d5194c3da021028
                  <br>
                  <input type="submit" name="submit" value="Valider" />
                </p>
@@ -30,49 +24,45 @@ echo '
 <!--<form action="formulaireajoutsuppr.php" method="post">-->
 <form action="protoajout.php" method="post">
     <div id="ajoutsuppr">
-<<<<<<< HEAD
-        Merci de bien vouloir remplir tous les champs :<br>
+        Merci de bien vouloir remplir tous les champs : <br>
 
-=======
->>>>>>> 3c4555815039a49af935fd054d5194c3da021028
-        <label for="Nom_plante">Nom plante :</label>
+        <label for="Nom_plante">Nom plante : </label>
         <input type="text" id="Nom_plante" name="Nom_plante" />
         <br>
 
-        <label for="Lieu">Lieu :</label>
+        <label for="Lieu">Lieu : </label>
         <input type="text" id="Lieu" name="Lieu"/>
         <br>
 
-        <label for="Latitude">Latitude :</label>
+        <label for="Latitude">Latitude : </label>
         <input type="text" id="Latitude" name="Latitude"/>
         <br>
 
-        <label for="Longitude">Longitude :</label>
+        <label for="Longitude">Longitude : </label>
         <input type="text" id="Longitude" name="Longitude"/>
         <br>
 
-<<<<<<< HEAD
-        <label for="date_relevé">Date_relevé :</label>
+        <label for="date_relevé">Date_relevé : </label>
         <input type="date" id="date_releve" name="date_releve"/>
-=======
-        <label for="date_relevé">Date_relevé (aaaa-mm-jj) :</label>
-        <input type="text" id="date_releve" name="date_releve"/>
->>>>>>> 3c4555815039a49af935fd054d5194c3da021028
+        <br>
+        
+        <label for="date_relevé">Date_relevé : </label>
+        <input type="date" id="date_releve" name="date_releve"/>
         <br>
 
-        <label for="URL_Photo">Photo (URL) :</label>
+        <label for="URL_Photo">Photo (URL) : </label>
         <input type="text" id="URL_Photo" name="URL_Photo"/>
         <br>
 
-        <label for="Nom_collecteur">Nom_collecteur :</label>
+        <label for="Nom_collecteur">Nom_collecteur : </label>
         <input type="text" id="Nom_collecteur" name="Nom_collecteur"/>
         <br>
 
-        <label for="Prénom_collecteur">Prénom_collecteur:</label>
+        <label for="Prénom_collecteur">Prénom_collecteur: </label>
         <input type="text" id="Prenom_collecteur" name="Prenom_collecteur"/>
         <br>
 
-        <label for="Commentaire">Commentaire(s) :</label>
+        <label for="Commentaire">Commentaire(s) : </label>
         <input type="text" id="Commentaire" name="Commentaire"/>
         <br>
 
