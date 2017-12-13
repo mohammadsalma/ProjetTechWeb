60ab2a3134a0c416bb34f9167cfb8e12f7ca2844
JulieCremaschi <33690438+JulieCremaschi@users.noreply.github.com>
2017-12-13 11:34:08 +0100

    Update listedonnees.php

--- listedonnees.php
+++ listedonnees.php
@@ -1,5 +1,4 @@
 <?php   
-
 	include "bdd.php";// inclusion du fichier a lire
 	include "entete.php";
 
@@ -7,24 +6,25 @@
       <fieldset>
         <legend>Identifiez-vous :</legend>
        <p>
-          <label for="login">Login :</label> 
+          <label for="login">Login : </label> 
           <input type="text" name="login" id="login" value="" />
-          <label for="password">Password :</label> 
-          <input type="password" name="password" id="password" value="" /> 
+          <label for="password">Password : </label> 
+          <input type="password" name="password" id="password" value="" />
+          <br> 
           <input type="submit" name="submit" value="Valider" />
         </p>
       </fieldset>
     </form></div><br>';
+    //formulaire de login
 
-	$base= connexionbd();// me permet de me connecter a la base de donnée car ele contient toutes les informations
+	$base= connexionbd();
+	// me permet de me connecter a la base de donnée car ele contient toutes les informations
 	$req= 'Select * from releves';
+
 	$requete=requete($base,$req);
-	//echo "$requete";
-	//print_r($requete);
 
 	// lecture du resultat
-	//echo'<html><head><meta charset="utf-8"/><title>Consultation de la BD</title><link rel="stylesheet" type="text/css" href="style.css"></head><body>';
-	echo'<table class="tabledonnees">';
+	echo'<table class="tabledonnees" id="tableaudonnees">';
 	echo'<thead><tr>
 			<th> Nom</th>
 			<th> Lieu</th>
@@ -38,10 +38,6 @@
 
 	// Pour acceder a des colonnes ligne/ligne(le echo dans la boucle)
 	foreach ($requete as $ligne) {
-
-		//echo $ligne['id'],$ligne['nom_plante'],$ligne['lieu'],$ligne['latitude'],$ligne['longitude'],$ligne['date_releve'],$ligne['photo'],$ligne['nom_collecteur'],$ligne['prenom_collecteur'],$ligne['commentaire'];
-	
-		//echo "<label>Nom: </label>";
 		echo '<tbody><tr>
 		<td>',$ligne['nom_plante'],' </td><td> ',
 		$ligne['lieu'],' </td><td> ',
@@ -52,18 +48,7 @@
 		$ligne['nom_collecteur'], ' </td><td> ',
 		$ligne['prenom_collecteur'], ' </td><td> ',
 		$ligne['commentaire'], ' </td>',
-		'</tr></tbody>';
-		
-		/*echo "<label>lieu: </label>";
-		echo $ligne['lieu'];
-		echo "<label>latitude: </label>";
-		echo $ligne['latitude'];
-		echo "<label>date_releve: </label>";
-		echo $ligne['date_releve'];
-		echo "<label>photo: </label>";
-		echo "<img src=",$ligne['photo'],">";*/
-	
-		//echo "<br>";
+		'</tr></tbody>';		
 	}
 
 	echo '</table>';
